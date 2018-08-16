4c718f8d4c990f9c8596acb6c375728cad470d04
Araz Sinjary <araz@Arazs-MacBook-Pro.local>
2018-08-16 19:54:08 +0300

    connecting the views

---
+++ DreamLister/View/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Araz Sinjary on 8/16/18.
+//  Copyright © 2018 Araz Sinjary. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+
+}
